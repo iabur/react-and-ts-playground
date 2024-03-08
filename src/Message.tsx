88ef419b439db3fc293d4eba75d4de661bffb8f0
iabur <iaburrahman@gmail.com>
2024-03-09 00:24:48 +0600

    initial commit

---
+++ src/Message.tsx
@@ -0,0 +1,9 @@
+function Message() {
+    const name = "iabur";
+    if (name === "iabur") {
+        return <h1>Name not allowed!</h1>;
+    }
+    return <h1>Hello {name}</h1>
+}
+
+export default Message;

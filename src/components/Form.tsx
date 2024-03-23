3fafeff7bee21bc2960ef02a0993e983029bba3e
iabur <iaburrahman@gmail.com>
2024-03-23 22:09:29 +0600

    Add form validation to name field in Form component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,17 @@
 import { FieldValues, useForm } from "react-hook-form";
 
+interface FormData {
+  name: String;
+  age: Number;
+}
+
 const Form = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
   const printData = (data: FieldValues) => {
     console.log(data);
   };
@@ -13,12 +23,19 @@ const Form = () => {
           Name
         </label>
         <input
-          {...register("name")}
+          {...register("name", { required: true, minLength: 3 })}
           id="name"
           type="text"
           className="form-control"
         />
+        {errors.name?.type === "required" && (
+          <p className="text-danger">Name is required</p>
+        )}
+        {errors.name?.type === "minLength" && (
+          <p className="text-danger">Name must be at least 3 characters</p>
+        )}
       </div>
+
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
@@ -30,6 +47,7 @@ const Form = () => {
           className="form-control"
         />
       </div>
+
       <button type="submit" className="btn btn-primary">
         Submit
       </button>
